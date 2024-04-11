331edfe1672f2bba6b6bd5ff0247b31644d7c6de
Jorge Macias <jmacias1@lafabril.com.ec>
2024-04-11 08:45:14 -0500

    first

---
+++ src/app.ts
@@ -0,0 +1,31 @@
+//console.log('Hola, mundo de nuevo')
+import { createBot, createFlow, MemoryDB, createProvider, addKeyword } from "@bot-whatsapp/bot"
+import { BaileysProvider, handleCtx } from "@bot-whatsapp/provider-baileys"
+
+//require ('dotenv').config()
+
+const flowBienvenida  = addKeyword ('Hola').addAnswer('Saludos, desde Bot')
+
+const main = async () => {
+    const provider = createProvider(BaileysProvider)
+
+    provider.initHttpServer(3002)
+
+    provider.http?.server.post('/send-message', handleCtx(async(bot,req,res) => {
+        const body = req.body
+        const phone = body.phone
+        const message = body.message
+        const mediaUrl = body.mediaUrl
+        await bot.sendMessage(phone, message, { 
+            media: mediaUrl
+        })
+        //res.end('esto es del servidor Polka')
+    }))
+    await createBot({
+        flow: createFlow([flowBienvenida]),
+        database: new MemoryDB(),
+        provider
+    })
+}
+
+main ()
